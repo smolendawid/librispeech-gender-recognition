3cf46742b9cf904cc9a0ac5947f71ff7e4d1d251
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-10-05 17:11:57 +0200

    checking file format and duration

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import connexion
+import librosa
 import numpy as np
 import soundfile as sf
+from checks import check_duration, check_format
 from voice_gender_classifier import VoiceGenderClassifier
 
 
 def recognize(audioFile):
+    check_format(audioFile)
     audio, fs = sf.read(audioFile)
+    check_duration(audio, fs)
     result, proba = vgc.predict(audio, fs)
     response = {"class": result, "probability": proba}
     return response
@@ -17,21 +21,6 @@ vgc = VoiceGenderClassifier(model_path, fs=16000)
 app = connexion.FlaskApp(__name__, port=9090, specification_dir='openapi/')
 app.add_api('gender_recognition-openapi.yaml')
 app.run()
-
-# {
-#     "class": "man",
-#     "probability": 0.43
-# }
-
-# {
-#     "class": "female",
-#     "probability": 0.43
-# }
-
-
-# "Too long recording"
-# "Too short "
-# "Wrong format "
 
 # ? stereo
 
