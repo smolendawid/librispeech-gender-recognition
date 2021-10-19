20c894e583f1a9562cc81a7e0cf3c936aebdef6d
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-10-19 11:28:36 +0200

    function correction

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 import connexion
-import numpy as np
-import soundfile as sf
-from checks import check_duration, check_format
 from voice_gender_classifier import VoiceGenderClassifier
+from audio_file_processing import processing
 
 
 def recognize(audioFile):
-    check_format(audioFile)
-    audio, fs = sf.read(audioFile)
-    check_duration(audio, fs)
-    result, proba = vgc.predict(audio, fs)
-    response = {"class": result, "probability": proba}
+    response = processing(audioFile, vgc)
     return response
 
 
@@ -20,5 +14,3 @@ vgc = VoiceGenderClassifier(model_path, fs=16000)
 app = connexion.FlaskApp(__name__, port=9090, specification_dir='openapi/')
 app.add_api('gender_recognition-openapi.yaml')
 app.run()
-
-# unit tests for all outputs and possible errors
