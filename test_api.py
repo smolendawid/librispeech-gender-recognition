426d87a782cfa9159712c09c5f29857a3cfacd17
GabrielaSmolen <gabrysiasmolen@gmail.com>
2021-10-19 11:30:02 +0200

    tests fixed

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from requests.api import patch
+
 
 def voice_gender_client(path):
 
@@ -9,19 +9,42 @@ def voice_gender_client(path):
         'Content-Type': 'multipart/form-data',
     }
 
+    file_format = path.split("/")[3].split(".")[1]
+
     files = {
-        'audioFile': (path, open(path, 'rb'), 'audio/wav'),
+        'audioFile': (path, open(path, 'rb'), 'audio' + '/' + file_format),
     }
 
     response = requests.post('http://localhost:9090/v1.0/recognition', files=files)
     return response
 
 
-def test(): 
-    path = 'data/raw/other/Gabi.wav'
-
+def test_normal_wave(): 
+    path = 'data/raw/other/Dawid2.wav'
     response = voice_gender_client(path=path)
-
-    # response = requests.post('http://localhost:9090/v1.0/recognition', files=dict(audioFile='data\raw\other\Gabi.wav'))
-
     assert response.status_code == 200
+
+
+def test_too_long_wave(): 
+    path = 'data/raw/other/Dawid.wav'
+    response = voice_gender_client(path=path)
+    assert response.status_code == 412
+
+
+def test_wrong_format():
+    path = 'data/raw/other/Gabi flac.flac'
+    response = voice_gender_client(path=path)
+    assert response.status_code == 415
+
+
+def test_too_short_wave(): 
+    path = 'data/raw/other/Short.wav'
+    response = voice_gender_client(path=path)
+    assert response.status_code == 412
+
+
+# def test(): 
+#     # path = 'data/raw/other/Gabi mp3.mp3'
+#         path = os.path.join('data/raw/other' + '/' + path)
+#         response = voice_gender_client(path=path)
+#         assert response.status_code == 200
